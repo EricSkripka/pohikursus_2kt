ad9ce44d7735b8eee7ce3d3b0f6c80c7c07942b1
Eric Skripka <EricSkripka@users.noreply.github.com>
2019-04-24 11:46:12 +0300

    Added interface AnalyticsInterface

---
+++ kontrolltoo2/src/main/java/kontrolltoo2/AnalyticsInterface.java
@@ -0,0 +1,10 @@
+package kontrolltoo2;
+
+interface AnalyticsInterface {
+
+    public void readFile();
+    public void splitFile();
+    public void lowerCase();
+    public void WordsWithCountMoreThanTwoILetters();
+
+}
