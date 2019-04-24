19d66daa20117af3f22369000e23789d4c974570
Eric Skripka <EricSkripka@users.noreply.github.com>
2019-04-24 12:12:16 +0300

    Updated Interface and added class file

---
+++ kontrolltoo2/src/main/java/kontrolltoo2/Analytics.java
@@ -0,0 +1,25 @@
+package kontrolltoo2;
+
+public class Analytics implements AnalyticsInterface {
+
+    @Override
+    public void readInput() {
+
+    }
+
+    @Override
+    public void splitInput() {
+
+    }
+
+    @Override
+    public void lowerCase() {
+
+    }
+
+    @Override
+    public void WordsWithCountMoreThanTwoILetters() {
+
+    }
+    
+}
--- kontrolltoo2/src/main/java/kontrolltoo2/AnalyticsInterface.java
+++ kontrolltoo2/src/main/java/kontrolltoo2/AnalyticsInterface.java
@@ -2,8 +2,8 @@ package kontrolltoo2;
 
 interface AnalyticsInterface {
 
-    public void readFile();
-    public void splitFile();
+    public void readInput();
+    public void splitInput();
     public void lowerCase();
     public void WordsWithCountMoreThanTwoILetters();
 
